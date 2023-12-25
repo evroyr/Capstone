54130aaba38525ac68d6762d540786e587fa2442
Antonio Roberts <141260347+evroyr@users.noreply.github.com>
2023-12-25 13:22:23 -0500

    api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
@@ -15,11 +16,53 @@ function render(state = store.Home) {
   router.updatePageLinks();
 }
 
+router.hooks({
+  before: (done, params) => {
+    // We need to know what view we are on to know what data to fetch
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      // New Case for the Home View
+      case "Home":
+        axios
+          // Get request to retrieve the current weather data using the API key and providing a city name
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.CAPSTONE_API}&q=orlando`
+          )
+          .then(response => {
+            // Convert Kelvin to Fahrenheit since OpenWeatherMap does not provide it directly
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+            // Create an object to be stored in the Home state from the response
+            store.Home.weather = {
+              city: response.data.name,
+              temp: kelvinToFahrenheit(response.data.main.temp),
+              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
+              description: response.data.weather[0].main
+            };
+
+            // Call render after the axios request is complete
+            render(store[view]);
+          })
+          .catch(error => {
+            // Handle errors
+            console.error("Error fetching data:", error);
+            done();
+          });
+        break; // Don't forget to add a break statement here
+    }
+  }
+});
+
 router
   .on({
     "/": () => render(),
     ":view": params => {
-      let view = capitalize(params.view); // Use params.view instead of params.data.view
+      let view = capitalize(params.view);
       if (view in store) {
         render(store[view]);
       } else {
