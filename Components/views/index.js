e2e625485663e1b5813aba94b9f206bdfd932741
Antonio Roberts <141260347+evroyr@users.noreply.github.com>
2024-01-01 19:14:57 -0500

    update

--- Components/views/index.js
+++ Components/views/index.js
@@ -2,4 +2,4 @@ export { default as About } from "./About";
 export { default as Home } from "./Home";
 export { default as Fasting } from "./Fasting";
 export { default as Workouts } from "./Workouts";
-export { default as Intermittent-Fasting } from "./Intermittent-Fasting";
+export { default as IntermittentFasting } from "./IntermittentFasting";
