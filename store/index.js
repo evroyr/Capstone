c346f67a819ffd589b4481d3ac47bcaa1d1d1b44
Antonio Roberts <141260347+evroyr@users.noreply.github.com>
2024-01-10 21:12:07 -0500

    fast

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 export { default as About } from "./About";
 export { default as Home } from "./Home";
-export { default as Fasting } from "./Fasting";
+export { default as Fasting } from "./fast";
 export { default as Viewnotfound } from "./Viewnotfound";
 export { default as Workouts } from "./Workouts";
 export { default as Links } from "./Links";
